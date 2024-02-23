739573fadf69720d78a0d4f34836084f98e4ed58
Seth Kin <154750880+sethkin@users.noreply.github.com>
2024-02-24 00:47:17 +0300

    Update lib.rs

--- src/icp_rust_boilerplate_backend/src/lib.rs
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
     extern crate serde;
     use candid::{Decode, Encode};
-    use ic_cdk::api::time;
+    use ic_cdk::api::time,
     use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
     use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
     use std::{borrow::Cow, cell::RefCell};
