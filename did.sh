4afecb01291bb218eb609d59909c77d5b89450d7
Gestas Dias <154750880+gestas-dias@users.noreply.github.com>
2024-02-24 14:17:37 +0300

    Update did.sh

--- did.sh
+++ did.sh
@@ -6,8 +6,8 @@ function generate_did() {
   canister_root="src/$canister"
 
   cargo build --manifest-path="$canister_root/Cargo.toml" \
-      --target wasm32-unknown-unknown \
-      --release --package "$canister" \
+      --target wasm32-unknown-unknown 
+      --release --package "$canister" 
 
   candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
 }
