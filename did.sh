9d587b2361f0f1bdf8bdab928c90588940ab8f71
Mert Lekeli <154750880+mertlekeli@users.noreply.github.com>
2024-02-28 22:58:13 +0300

    Update did.sh

--- did.sh
+++ did.sh
@@ -7,7 +7,7 @@ function generate_did() {
 
   cargo build --manifest-path="$canister_root/Cargo.toml" \
       --target wasm32-unknown-unknown 
-      --release --package "$canister" 
+      --release --package "canister" 
 
   candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
 }
